c88c02440e3382d70ce2d11cc3fe1705c9e6ffc3
scheung38 <sebastian_cheung@yahoo.com>
2019-01-15 17:59:20 +0000

    add graphql endpoint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-const { buildSchema }  = require('graphql')
+const { buildSchema } = require('graphql')
 
 const app = express();
 
@@ -14,8 +14,32 @@ app.use(bodyParser.json());
 // })
 
 app.use('graphql', graphqlHttp({
-    schema: null,
-    rootValue
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+
+    }
+
+    type RootMutation {
+        createEvent(name: String): String
+
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+        },
+        createEvent:(args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
 }));
 
 
