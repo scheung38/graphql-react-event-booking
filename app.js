fe6d2088fa3e2234f2314b1e571e36783e738bfd
scheung38 <sebastian_cheung@yahoo.com>
2019-01-15 17:28:28 +0000

    add buildSchema from graphql

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const bodyParser  = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema }  = require('graphql')
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(3000);
 
-app.get('/', (err, res) => {
-    res.send('Hello World!');
-})
+
+// app.get('/', (err, res) => {
+//     res.send('Hello World!');
+// })
+
+app.use('graphql', graphqlHttp({
+    schema: null,
+    rootValue
+}));
+
+
+app.listen(3000);
